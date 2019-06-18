022944fa2eb3a03149eb32f1d59f26ec9c7ba8ae
Unknown <nameimprobable@gmail.com>
2019-06-18 21:53:33 +0300

    login route

--- app/middleware/router-auth.js
+++ app/middleware/router-auth.js
@@ -1,10 +1,10 @@
 export default function({ store, redirect, route }) {
-    store.state.user != null && route.name == 'login' ? redirect('/admin') : ''
-    !store.state.user && isAdminRoute(route) ? redirect('/login') : ''
+  store.state.user != null && route.name == 'auth' ? redirect('/admin') : ''
+  !store.state.user && isAdminRoute(route) ? redirect('/auth') : ''
+}
+
+function isAdminRoute(route) {
+  if (route.matched.some(record => record.path == '/admin')) {
+    return true
   }
-  
-  function isAdminRoute(route) {
-    if (route.matched.some(record => record.path == '/admin')) {
-      return true
-    }
-  }
+}
