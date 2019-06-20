e3b1526e6251954e63b66e27c596f95fa18ac6ea
Unknown <nameimprobable@gmail.com>
2019-06-20 09:53:22 +0300

    modular firebase

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -11,26 +11,9 @@ import { mapGetters } from 'vuex'
 export default {
   components: {
     TheFooter
-  },
-  computed: {
-    ...mapGetters(['isLoaded'])
   }
 }
 </script>
 
-<style scoped>
-.wrapper {
-  width: 100vw;
-  min-height: 100vh;
-  /* display: flex;
-  align-items: center;
-  justify-content: center; */
-  background: #fff;
-  padding: 32px;
-}
-
-.wrapper-loaded {
-  background: #fafafa;
-  padding-top: 0;
-}
+<style>
 </style>
--- app/store/user.js
+++ app/store/user.js
@@ -1,14 +1,9 @@
-// import cloneDeep from 'clone-deep'
-
 export const state = () => ({
   user: null
 })
 
 export const mutations = {
   setUser(state, user) {
-    // state.user = Object.assign({}, user)
     state.user = JSON.parse(JSON.stringify(user))
-    // const copy = cloneDeep(user)
-    // state.user = copy
   }
 }
